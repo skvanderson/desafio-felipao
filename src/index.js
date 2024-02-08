8984ffff677bba49b9672a3141e5dc68d2e1d934
skvanderson <sharlleskrueger@gmail.com>
2024-02-08 16:38:41 -0300

    DESAFIO CONCLUÍDO

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
-let nome = "Sharlles";
-console.log("teste1");
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Digite o nome do herói: ', (nomeHeroi) => {
+  rl.question('Digite a quantidade de experiência do herói: ', (xp) => {
+    xp = parseInt(xp);
+
+    let nivel;
+    if (xp < 1000) {
+        nivel = "Ferro";
+    } else if (xp >= 1001 && xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp >= 2001 && xp <= 5000) {
+        nivel = "Prata Ouro";
+    } else if (xp >= 6001 && xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp >= 5001 && xp <= 8000) {
+        nivel = "Platina Diamante";
+    } else if (xp >= 8001 && xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp >= 9001 && xp <= 10000) {
+        nivel = "Imortal";
+    } else {
+        nivel = "Radiante";
+    }
+    console.log(`O Herói ${nomeHeroi} está no nível de ${nivel}`);
+
+    rl.close();
+  });
+});
